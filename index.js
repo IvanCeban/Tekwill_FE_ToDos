0de3bb50fea326d516a77d0cbc48a6b03087738e
ivan.ceban <ivan.ceban@helloprint.com>
2023-10-20 17:39:20 +0300

    Checkbox added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const button = document.getElementById('get_todos');
+  const checkbox = document.getElementById('checkbox2');
 
   // Add event listener
   if (button) {
@@ -7,9 +8,20 @@ document.addEventListener('DOMContentLoaded', function () {
     button.addEventListener('click', handleClick);
   }
 
+  if (checkbox) {
+    checkbox.addEventListener('change', handleCheckboxChange);
+  }
   // Event handler function
   function handleClick(event) {
     event.preventDefault();
     console.log('Button clicked!');
   }
+
+  function handleCheckboxChange() {
+    console.log('Checked', this.checked);
+  }
 });
+
+// 1. Get real users list from https://jsonplaceholder.typicode.com/
+// 2. When setting user ID get this user ToDos
+// 3. When checking a todo, set it's status to done and send the "completed" value to server using PUT
